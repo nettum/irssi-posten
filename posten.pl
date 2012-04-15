589ee8c0b0bc44a5a74d9d93fbf25c4c1bc1de73
Marius Nettum <marius@internettum.no>
2012-04-15 16:52:00 +0200

    * Fixed bug with regex * Also print pacakges that isn't in a packageSet * Only print postalcode and city if present

--- posten.pl
+++ posten.pl
@@ -18,21 +18,34 @@ $bringpackageurl = 'http://sporing.bring.no/sporing.json?q=';
 
 sub trackPackage() {
 	my ($server, $msg, $nick, $addr, $target) = @_;
-	if ($msg =~ m/^^!sporing ([\w-]*)/) {
+	if ($msg =~ m/^!sporing ([\w-]*)/) {
 		my $content = get($bringpackageurl . $1);
 		if (defined($content)) {
+			my $strip = HTML::Strip->new();
 			my $trackinginfo = decode_json($content);
 			if (defined($trackinginfo)) {
+				my $address = '';
 				if (defined($trackinginfo->{'consignmentSet'}->[0]->{'error'}->{'message'})) {
 					$server->command('msg ' . $target . ' ' . $trackinginfo->{'consignmentSet'}->[0]->{'error'}->{'message'});
 				} elsif (defined($trackinginfo->{'consignmentSet'}->[0]->{'packageSet'})) {
-					my $strip = HTML::Strip->new();
 					for my $event ( @{$trackinginfo->{'consignmentSet'}->[0]->{'packageSet'}->[0]->{'eventSet'}} ) {
 						if ($event->{'status'} ne 'NOTIFICATION_SENT') {
-							my $cleantext = $event->{'displayDate'} . ' ' . $event->{'displayTime'} . ', ' . $event->{'postalCode'} . ' ' . $event->{'city'} . ': ' . $strip->parse($event->{'description'});
+							if ($event->{'postalCode'} ne '' && $event->{'city'} ne '') {
+								$address = ', ' . $event->{'postalCode'} . ' ' . $event->{'city'};
+							}
+							my $cleantext = $event->{'displayDate'} . ' ' . $event->{'displayTime'} . $address . ': ' . $strip->parse($event->{'description'});
 							$strip->eof;
 							$server->command('msg ' . $target . ' ' . $cleantext);
 						}
+					}
+				} elsif (defined($trackinginfo->{'consignmentSet'}->[0]->{'eventSet'})) {
+					for my $event ( @{$trackinginfo->{'consignmentSet'}->[0]->{'eventSet'}} ) {
+						if ($event->{'postalCode'} ne '' && $event->{'city'} ne '') {
+							$address = ', ' . $event->{'postalCode'} . ' ' . $event->{'city'};
+						}
+						my $cleantext = $event->{'displayDate'} . ' ' . $event->{'displayTime'} . $address . ': ' . $strip->parse($event->{'description'});
+						$strip->eof;
+						$server->command('msg ' . $target . ' ' . $cleantext);
 					}
 				}
 
